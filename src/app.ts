1abe23da8ad96b33cef7fd2138ab7f5ca61a6414
Eliska Tinterova <s00273768@atu.ie>
2026-09-16 10:03:34 +0100

    file added app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, {Application, Request, Response} from "express";
+
+const PORT = process.env.PORT||3000;
+const app: Application = express();
+
+app.get("/ping", async (_req : Request, res: Response) => {
+    res.json({
+        message: "Hello from Una"
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log("Server is running on port", PORT);
+});
