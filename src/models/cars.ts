ab2459e048ce923eb7d6fc8a6127f6cb3a5abb49
Eliska Tinterova <s00273768@atu.ie>
2026-09-23 10:33:17 +0100

    car model set up (car.ts )

---
+++ src/models/cars.ts
@@ -0,0 +1,14 @@
+import {Schema, model} from 'mongoose';
+
+export interface ICar{
+    make : string;
+    model : string;
+}
+
+const carSchema = new Schema <ICar>(
+    {
+        make : {type: String, required :true},
+        model : {type: String, required : true},
+    },
+    {timestamps: true}
+);
